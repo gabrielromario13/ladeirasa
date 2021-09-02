8d6a675c47833a9b5437adec2b9f78d5547847d0
Eduardo Haupt <brhbs0963@ambev.com.br>
2021-09-02 18:36:48 -0300

    s

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import { Container } from "./styles";
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
-const Card = ({ className, children }) => {
-  return <Container className={className}>{children}</Container>;
-};
+export default function MediaCard() {
+  const classes = useStyles();
 
-export default Card;
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://pbs.twimg.com/profile_images/1049357568420843520/DqRJemT7_400x400.jpg"
+          title="Contemplative Reptile"
+        />
+      </CardActionArea>
+      <CardActions>
+        <h6>{`Shape Romario \n R$: 120,00`}</h6> 
+        <h6></h6>
+      </CardActions>
+    </Card>
+  );
+}
+
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import MediaCard from '../../components/Card'
 
 const Products = () => {
   const { category, subCategory } = useParams()
   return (
     <div>
       PRODUTO: {category} subProduto: {subCategory}
+      <MediaCard />
     </div>
   )
 }
